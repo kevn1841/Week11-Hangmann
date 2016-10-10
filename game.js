e6936c95279450e8efb3bbd524fd64e8f0f2b43e
Kevin Shukla <kevn1841@gmail.com>
2016-10-10 04:27:51 -0400

    word.js started

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
 //The word bank
 exports.game = {
-	wordBank : ["heart and soul", "relax", "burning up", "manic monday", "into the groove", "invisible touch", "rebel yell"]
+	words : ["heart and soul", "relax", "burning up", "manic monday", "into the groove", "invisible touch", "rebel yell"]
 };
