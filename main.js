efd6ccdb9be2899a25707ca7ab6af5c92c658b09
Kevin Shukla <kevn1841@gmail.com>
2016-10-10 04:29:45 -0400

    cleaned up code

--- main.js
+++ main.js
@@ -26,25 +26,22 @@ game = {
 		var self = this;
 
 		prompt.get(['guessLetter'], function(err, result) {
-		    // result is an object like this: { guessLetter: 'f' }
-		    //console.log(result);
+
 		    
 		    console.log('  The letter or space you guessed is: ' + result.guessLetter);
 
-		    //this checks if the letter was found and if it is then it sets that specific letter in the word to be found
+		    
 		    var findHowManyOfUserGuess = self.currentWrd.checkIfLetterFound(result.guessLetter);
 
-		    //if the user guessed incorrectly minus the number of guesses they have left
 		    if (findHowManyOfUserGuess == 0){
 		    	console.log('You guessed wrong!');
 		    	self.guessesRemaining--;
 		    }else{
 		    	console.log('You guessed right!');
 
-		    	//check if you win only when you are right
 	    		if(self.currentWrd.didWeFindTheWord()){
 			    	console.log('You Won!!!');
-			    	return; //end game
+			    	return;
 			    }
 		    }
 		    
